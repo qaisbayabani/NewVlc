3eae4213710304132b967e2a4cac9dd72808c88c
qaisbayabani <qaisbayabani2@gmail.com>
2022-04-02 00:51:46 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using LibVLCSharp.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NewVlc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Core.Initialize();
+
+            var libVLC = new LibVLC();
+
+            var media = new Media(libVLC, "tcp://127.0.0.1:27184", FromType.FromLocation);
+
+            //var media = new Media(libVLC, "e:\\Risen.mp4");
+           // var media = new Media(libVLC, "http://127.0.0.1:27183", FromType.FromLocation);
+
+            var mp = new MediaPlayer(media);
+            
+            Console.WriteLine(media.Duration);
+
+
+            mp.Play();
+
+            
+
+            Console.ReadKey();
+        }
+    }
+}
